f0c4f61c95413debbf6eb4cc93c88a0de35558c2
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2014-03-09 12:12:41 -0600

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sportswear::Application.config.secret_key_base = '6d4e9b7ba5496edecb2a7ed23f2b117557ad2dfb3abccf4cc1b27b7543fcc3d8902bfed95a6a29e828f5a822a1fc4589370aed7783ddd5c9483139ab2e307599'
