dba9e5d6f1698f6c2d532e7641194f996cd0c321
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2014-03-09 21:27:20 -0600

    Quitando (temporalmente) el simlink al archivo de secret token

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :ssh_options, {
 set :log_level, :debug
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
+set :linked_files, %w{config/database.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
