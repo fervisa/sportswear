3b4288054ba9a0acb1f7b3a45ea3f9f630d0750d
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2014-03-10 20:54:50 -0600

    Quitando el guest checkout

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -10,6 +10,7 @@ Spree.config do |config|
   # Uncomment to override the default site name.
   config.site_name = "Sportswear - Ropa deportiva"
   config.allow_ssl_in_production = false
+  config.allow_guest_checkout = false
 end
 
 Spree.user_class = "Spree::User"
