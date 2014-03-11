c83eacfeae59d8ade318fe736bf59c243c4f64f2
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2014-03-10 23:13:44 -0600

    Agregado directorio de subida de spree a linked_dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :linked_files, %w{config/database.yml config/initializers/devise.rb}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-set :linked_dirs, %w{bin log tmp vendor/bundle public/system public/uploads}
+set :linked_dirs, %w{bin log tmp vendor/bundle public/system public/uploads public/spree}
 
 SSHKit.config.command_map[:rake]  = "bundle exec rake"
 SSHKit.config.command_map[:rails] = "bundle exec rails"
